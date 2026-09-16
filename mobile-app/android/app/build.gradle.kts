483b08397eeb1d5da73ef3a2cbae6a32b9b58c83
Andreeee03 <catt.andri@gmail.com>
2026-09-16 13:51:04 +0200

    Configure release APK signing

--- mobile-app/android/app/build.gradle.kts
+++ mobile-app/android/app/build.gradle.kts
@@ -1,6 +1,15 @@
-﻿plugins {
+import java.util.Properties
+import java.io.FileInputStream
+
+plugins {
     id("com.android.application")
     id("dev.flutter.flutter-gradle-plugin")
+}
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 
 android {
@@ -58,9 +67,18 @@ android {
         }
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+
     buildTypes {
         release {
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
